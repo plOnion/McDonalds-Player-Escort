4206776b5da844b715e3ef6fb2de3cec64e8c640
plOnion <damian.lukasiewicz@gmail.com>
2019-03-03 13:11:24 +0100

    fixed problems with rooms gallery

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ imgs.forEach((img)=>{
     img.addEventListener("click", (e)=>{
         const modal = document.createElement("div");
         const modalImg = e.target.cloneNode(false);
+        const position = window.scrollY;
+        console.log(position);
         modal.classList.add("modal-img-card");
+        modal.style.top = "" + position + "px";
         modalImg.classList.add("modal-img");
         modal.appendChild(modalImg);
         document.body.appendChild(modal);
-        console.log(modal);
         modalImg.addEventListener("click", ()=>{
             modal.parentNode.removeChild(modal);
         })
